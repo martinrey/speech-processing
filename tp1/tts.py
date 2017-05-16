0d7c04c1ce3ad7cb003b4a5c79990acab9de85e3
Martin Caravario <mcaravario@dc.uba.ar>
2017-05-16 19:50:28 -0300

    Avanzado el script de python

--- tp1/tts.py
+++ tp1/tts.py
@@ -18,7 +18,7 @@ def separar_en_difonos(string):
 
 def main():
     string = sys.argv[1]
-    # output_dir = sys.argv[2]
+    output_dir = sys.argv[2]
 
     pregunta = False
 
@@ -33,12 +33,27 @@ def main():
     # Creamos script de praat que abra todos los archivos necesarios y los seleccione
     for i, difono in enumerate(difonos):
          # Selecciono el difono
-         archivo.write("Read from file: " + '"audios/difonos/' + str(difono) + '"' + "\n")
+         archivo.write("Read from file: " + '"audios/difonos/' + str(difono) + '.wav"' + "\n")
 
          # Renombro difono
-         archivo.write("selectObject: " + str(difono) +  ' Rename: "difono' + str(i) +'"' + "\n" )
+         archivo.write('selectObject: "Sound  '+ str(difono) + '"' + "\n")
+         archivo.write('Rename: "difono' + str(i) +'"' + "\n" )
 
-    # Ejecutamos el script de praat
-    # os.system('praat concatenar.praat')
+    # Selecciono todos los archivos
+    archivo.write("select Sound difono0 \n")
+    i = 1
+    while(i<len(difonos)):
+        archivo.write("plus Sound difono"+str(i)+"\n")
+        i += 1
+
+    # Concateno todos los archivos abiertos
+    archivo.write("Concatenate recoverably \n")
+
+    # Guardo el audio generado.
+    archivo.write("Save as WAV file: " + str(output_dir))
+    archivo.close()
+
+    #  Ejecutamos el script de praat
+    os.system('praat concatenar.praat')
 
 main()
