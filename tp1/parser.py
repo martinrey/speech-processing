f88927b7ae6907f0061d6e72270d0b48611cad71
Martin Caravario <mcaravario@dc.uba.ar>
2017-05-28 16:37:27 -0300

    Vuelta al commit anterior

---
+++ tp1/parser.py
@@ -0,0 +1,38 @@
+import re
+import fileinput
+
+
+class Parser(object):
+    def parse(self):
+        raise NotImplementedError('Subclass responsibility!')
+
+
+class PitchParser(Parser):
+    def parse(self, filename):
+        """" Metodo que parsea el archivo que contiene el pitch track y aumenta los ultimos valores para modificar la prosodia"""
+        points_size = 0
+        point_index = -1
+        for line in fileinput.input(filename, inplace=True,  backup='.bak'):
+            entre = False
+            m = re.search('points: size = (.+?)$', str(line))
+            if m:
+                points_size = int(m.group(1))
+
+            if points_size > 1:
+
+                n = re.search('points \[(.+?)\]:', str(line))
+                if n:
+                    point_index = int(n.group(1))
+
+                # Para modificar el Pitch se toman los ultimos 6 valores del pitch y se los incrementa
+                # en un valor de 25% con respecto a su valor anterior y de forma paulatina.
+                if point_index >= points_size - 6:
+                    p = re.search('value = (.+?)$', str(line))
+                    if p:
+                        point_value = p.group(1)
+                        point_value = float(point_value)
+                        point_value += (point_index * 25) * point_value / 100
+                        print('    value = ' + str(point_value))
+                        entre = True
+            if not entre:
+                print(line, end="")
---
+++ tp1/tts.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+from parser import PitchParser
+import sys
+import os
+
+def separar_en_difonos(string):
+    res = []
+    primero = string[0]
+    ultimo = string[-1]
+    res.append("-"+primero)
+
+    for i in range(len(string)-1):
+        res.append(string[i]+string[i+1])
+
+    res.append(ultimo+"-")
+
+    return res
+
+def main():
+    string = sys.argv[1]
+    # Separamos en el nombre del archivo y el path a donde se guardara
+    output_dir = sys.argv[2]
+    output_name = os.path.basename(output_dir)[:-4]
+
+    pregunta = False
+    if(string[-1] == '?'):
+        pregunta = True
+        string = string[:-1]
+
+
+    difonos = separar_en_difonos(string)
+    archivo = open("concatenar.praat", "w")
+    os.system("mkdir tmp")
+    i = 0
+    # Creamos script de praat que abra todos los archivos necesarios y los seleccione
+    while i < len(difonos):
+        if(pregunta and 'A' in difonos[i]):
+            # Extraccion del pitch
+            os.system('praat scripts/extraer-pitch-track.praat ../audios/difonos/' + str(difonos[i]) + '.wav ../tmp/' + str(difonos[i]) + '.PitchTier 50 300')
+            # Modifico Pitch
+            pitch_parser = PitchParser()
+            pitch_parser.parse(filename="tmp/" + str(difonos[i]) + '.PitchTier')
+            # Resintetizamos audio
+            os.system('praat scripts/reemplazar-pitch-track.praat ../audios/difonos/' + str(difonos[i]) +'.wav ../tmp/' + str(difonos[i]) + '.PitchTier ../tmp/' + str(difonos[i]) +'.wav 50 300')
+
+        else:
+            os.system("cp audios/difonos/" + str(difonos[i]) +".wav tmp/"   )
+
+        # Selecciono el difono
+        archivo.write("Read from file: " + '"tmp/' + str(difonos[i]) + '.wav"' + "\n")
+
+        # Renombro difono
+        archivo.write('selectObject: "Sound '+ str(difonos[i]) + '"' + "\n")
+        archivo.write('Rename: "difono' + str(i) +'"' + "\n" )
+        i += 1
+    # Selecciono todos los archivos
+    archivo.write("select Sound difono0\n")
+    i = 1
+    while(i<len(difonos)):
+        archivo.write("plus Sound difono"+str(i)+"\n")
+        i += 1
+
+    # Concateno todos los archivos abiertos
+    archivo.write("Concatenate recoverably\n")
+
+    # Selecciono el archivo concatenado
+    archivo.write("select Sound chain\n")
+
+
+    # Guardo el audio generado
+    archivo.write('Save as WAV file: "' + str(output_dir) + '"')
+
+    archivo.close()
+
+    #  Ejecutamos el script de praat
+    os.system('praat concatenar.praat')
+
+    if(pregunta):
+        # Extraemos pitch
+        os.system('praat scripts/extraer-pitch-track.praat ' + str(output_dir) + " ../tmp/" + str(output_name) + '.PitchTier 50 300')
+        # Modificamos pitch
+        pitch_parser = PitchParser()
+        pitch_parser.parse(filename="tmp/" + str(output_name)+'.PitchTier')
+        # Resintetizamos audio
+        os.system('praat scripts/reemplazar-pitch-track.praat ' + str(output_dir) +' ../tmp/' + str(output_name) + '.PitchTier '
+                    + str(output_dir) +' 50 300')
+
+    os.system("rm  concatenar.praat")
+    os.system("rm -rf tmp/")
+main()
