f95ba9e410b37b027fc2120bd32b4545d8b0281e
Martin Rey <marto.rey2006@gmail.com>
2017-05-28 08:55:37 -0300

    Notebook explicando el tp

--- tp1/notebooks/parser.py
+++ tp1/notebooks/parser.py
@@ -9,7 +9,6 @@ class Parser(object):
 
 class PitchParser(Parser):
     def parse(self, filename):
-        """" Metodo que parsea el archivo que contiene el pitch track y aumenta los ultimos valores para modificar la prosodia"""
         points_size = 0
         point_index = -1
         for line in fileinput.input(filename, inplace=True,  backup='.bak'):
@@ -24,15 +23,20 @@ class PitchParser(Parser):
                 if n:
                     point_index = int(n.group(1))
 
-                # Para modificar el Pitch se toman los ultimos 6 valores del pitch y se los incrementa
-                # en un valor de 25% con respecto a su valor anterior y de forma paulatina.
-                if point_index >= points_size - 6:
+                if point_index == points_size - 1 or point_index == points_size:
                     p = re.search('value = (.+?)$', str(line))
                     if p:
                         point_value = p.group(1)
                         point_value = float(point_value)
-                        point_value += (point_index * 25) * point_value / 100
+                        point_value += 45 * point_value / 100
+                        # line = 'value = ' + str(point_value)
                         print('    value = ' + str(point_value))
                         entre = True
             if not entre:
                 print(line, end="")
+
+# if len(points) > 1:
+#            points[-1] += 45 * points[-1] / 100
+#            points[-2] += 45 * points[-2] / 100
+#        elif len(points) == 1:
+#            points[-1] += 45 * points[-1] / 100
--- tp1/src/tts.py
+++ tp1/src/tts.py
@@ -1,21 +1,23 @@
-#!/usr/bin/env python3
-
-from parser import PitchParser
-import sys
 import os
+import sys
+
+from parsers import PitchParser
+import plotly
+
 
 def separar_en_difonos(string):
     res = []
     primero = string[0]
     ultimo = string[-1]
-    res.append("-"+primero)
+    res.append("-" + primero)
 
-    for i in range(len(string)-1):
-        res.append(string[i]+string[i+1])
+    for i in range(len(string) - 1):
+        res.append(string[i] + string[i + 1])
 
-    res.append(ultimo+"-")
+    res.append(ultimo + "-")
 
     return res
+
 
 def main():
     string = sys.argv[1]
@@ -24,10 +26,9 @@ def main():
     output_name = os.path.basename(output_dir)[:-4]
 
     pregunta = False
-    if(string[-1] == '?'):
+    if (string[-1] == '?'):
         pregunta = True
         string = string[:-1]
-
 
     difonos = separar_en_difonos(string)
     archivo = open("concatenar.praat", "w")
@@ -35,30 +36,33 @@ def main():
     i = 0
     # Creamos script de praat que abra todos los archivos necesarios y los seleccione
     while i < len(difonos):
-        if(pregunta and 'A' in difonos[i]):
+        if (pregunta and 'A' in difonos[i]):
             # Extraccion del pitch
-            os.system('praat scripts/extraer-pitch-track.praat ../audios/difonos/' + str(difonos[i]) + '.wav ../tmp/' + str(difonos[i]) + '.PitchTier 50 300')
+            os.system(
+                'praat scripts/extraer-pitch-track.praat ../audios/difonos/' + str(difonos[i]) + '.wav ../tmp/' + str(
+                    difonos[i]) + '.PitchTier 50 300')
             # Modifico Pitch
             pitch_parser = PitchParser()
             pitch_parser.parse(filename="tmp/" + str(difonos[i]) + '.PitchTier')
             # Resintetizamos audio
-            os.system('praat scripts/reemplazar-pitch-track.praat ../audios/difonos/' + str(difonos[i]) +'.wav ../tmp/' + str(difonos[i]) + '.PitchTier ../tmp/' + str(difonos[i]) +'.wav 50 300')
+            os.system('praat scripts/reemplazar-pitch-track.praat ../audios/difonos/' + str(
+                difonos[i]) + '.wav ../tmp/' + str(difonos[i]) + '.PitchTier ../tmp/' + str(difonos[i]) + '.wav 50 300')
 
         else:
-            os.system("cp audios/difonos/" + str(difonos[i]) +".wav tmp/"   )
+            os.system("cp audios/difonos/" + str(difonos[i]) + ".wav tmp/")
 
         # Selecciono el difono
         archivo.write("Read from file: " + '"tmp/' + str(difonos[i]) + '.wav"' + "\n")
 
         # Renombro difono
-        archivo.write('selectObject: "Sound '+ str(difonos[i]) + '"' + "\n")
-        archivo.write('Rename: "difono' + str(i) +'"' + "\n" )
+        archivo.write('selectObject: "Sound ' + str(difonos[i]) + '"' + "\n")
+        archivo.write('Rename: "difono' + str(i) + '"' + "\n")
         i += 1
     # Selecciono todos los archivos
     archivo.write("select Sound difono0\n")
     i = 1
-    while(i<len(difonos)):
-        archivo.write("plus Sound difono"+str(i)+"\n")
+    while (i < len(difonos)):
+        archivo.write("plus Sound difono" + str(i) + "\n")
         i += 1
 
     # Concateno todos los archivos abiertos
@@ -66,7 +70,6 @@ def main():
 
     # Selecciono el archivo concatenado
     archivo.write("select Sound chain\n")
-
 
     # Guardo el audio generado
     archivo.write('Save as WAV file: "' + str(output_dir) + '"')
@@ -76,16 +79,20 @@ def main():
     #  Ejecutamos el script de praat
     os.system('praat concatenar.praat')
 
-    if(pregunta):
+    if (pregunta):
         # Extraemos pitch
-        os.system('praat scripts/extraer-pitch-track.praat ' + str(output_dir) + " ../tmp/" + str(output_name) + '.PitchTier 50 300')
+        os.system('praat scripts/extraer-pitch-track.praat ' + str(output_dir) + " ../tmp/" + str(
+            output_name) + '.PitchTier 50 300')
         # Modificamos pitch
         pitch_parser = PitchParser()
-        pitch_parser.parse(filename="tmp/" + str(output_name)+'.PitchTier')
+        pitch_parser.parse(filename="tmp/" + str(output_name) + '.PitchTier')
         # Resintetizamos audio
-        os.system('praat scripts/reemplazar-pitch-track.praat ' + str(output_dir) +' ../tmp/' + str(output_name) + '.PitchTier '
-                    + str(output_dir) +' 50 300')
+        os.system('praat scripts/reemplazar-pitch-track.praat ' + str(output_dir) + ' ../tmp/' + str(
+            output_name) + '.PitchTier '
+                  + str(output_dir) + ' 50 300')
 
     os.system("rm  concatenar.praat")
     os.system("rm -rf tmp/")
+
+
 main()
