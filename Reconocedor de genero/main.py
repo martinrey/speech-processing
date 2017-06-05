a35420ac293774db536517d694cc01e8368ef6f6
Martin Rey <marto.rey2006@gmail.com>
2017-06-05 20:21:12 -0300

    empiezo con la idea de agrandar el set de datos

--- Reconocedor de genero/main.py
+++ Reconocedor de genero/main.py
@@ -23,20 +23,30 @@ from sklearn.base import BaseEstimator
 class Reshape(BaseEstimator):
     def fit(self, X, y=None):
         return self
+
     def transform(self, X, y=None):
-        return X.reshape((150, 32, 1))
+        return X.reshape((X.shape[0], X.shape[1], 1))
 
 
 def create_model(optimizer='rmsprop', init='glorot_uniform'):
     # CREO MIS CONVOLUCIONES
     model = Sequential()
-    model.add(Convolution1D(16, 3, kernel_initializer=init, activation='relu', input_shape=(32, 1)))
-    model.add(Convolution1D(32, 3, kernel_initializer=init, activation='relu'))
+    model.add(Convolution1D(16, 5, strides=1, padding='same', kernel_initializer=init, activation='relu',
+                            input_shape=(131072, 1)))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Convolution1D(32, 5, strides=1, padding='same', kernel_initializer=init, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Convolution1D(64, 5, strides=1, padding='same', kernel_initializer=init, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Convolution1D(128, 5, strides=1, padding='same', kernel_initializer=init, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Convolution1D(256, 5, strides=1, padding='same', kernel_initializer=init, activation='relu'))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Dropout(0.25))
     model.add(Flatten())
+    model.add(Dense(512, kernel_initializer=init, activation='relu'))
+    model.add(Dense(256, kernel_initializer=init, activation='relu'))
     model.add(Dense(128, kernel_initializer=init, activation='relu'))
-    model.add(Dense(64, kernel_initializer=init, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1, activation='sigmoid'))
     # Compile model
@@ -45,7 +55,7 @@ def create_model(optimizer='rmsprop', init='glorot_uniform'):
 
 
 def main():
-    RUTA_DIRECTORIO_DATOS = "datos"
+    RUTA_DIRECTORIO_DATOS = "../datos"
     VENTANA_EN_SEGUNDOS = 10
     SAMPLE_RATE = 16000
     CANTIDAD_DE_FRAMES_A_PROCESAR = 131072
@@ -74,7 +84,6 @@ def main():
             y.append(0)
     y = np.asarray(y)
 
-
     X, y = shuffle(X, y, random_state=0)
     X_train = X[:150]
     X_test = X[150:180]
@@ -87,15 +96,28 @@ def main():
     BATCHES = [10]
     INIT = ['glorot_uniform']
 
+    #pca = PCA(n_components=32)
+    #wtf = pca.fit_transform(X_train)
+
     # create model
     model = KerasClassifier(build_fn=create_model, verbose=0)
 
-    estimators = [("reduce_dim", PCA(n_components=32)), ("reshaper", Reshape()), ('clf', model)]
+    estimators = [("reshaper", Reshape()), ('clf', model)]
     pipeline = Pipeline(estimators)
 
-    pipeline.fit(X_train, y_train)
-    score = pipeline.score(X_train, y_train)
-    print(score)
+    param_grid = [
+        {
+            'clf__optimizer': OPTIMIZERS,
+            'clf__epochs': EPOCHS,
+            'clf__batch_size': BATCHES,
+            'clf__init': INIT
+        },
+    ]
+
+    grid_search = GridSearchCV(estimator=pipeline, param_grid=param_grid)
+    grid_result = grid_search.fit(X_train, y_train)
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+
 main()
-
-
