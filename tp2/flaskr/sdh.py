d6003d3651f578a695ee4ab2a58013aa550b0e69
Martin Caravario <mcaravario@dc.uba.ar>
2017-07-09 23:50:38 -0300

    Agrego mensaje intro

--- tp2/flaskr/sdh.py
+++ tp2/flaskr/sdh.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # coding=utf-8
-
+import os
 from sdh.secciones.seccion_finalizar import SeccionFinalizar
 from sdh.secciones.seccion_informacion import SeccionInformacion
 from sdh.secciones.seccion_reserva import SeccionReserva
 from sdh.paso.paso import Paso
 
 if __name__ == '__main__':
+    os.system("play sounds/pregunta_maquina_intro.wav")
     Paso(path_a_audio_de_mensaje_inicial="sounds/pregunta_maquina_inicial.wav",
          path_a_audio_de_mensaje_erroneo="sounds/error_seleccion_de_servicio.wav",
          secciones=[SeccionReserva, SeccionInformacion, SeccionFinalizar]).comenzar()
